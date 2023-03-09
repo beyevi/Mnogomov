2e92842958994759d0eb42f87f2d7c59fc1dfd72
Eugene Bewz <bevz.pn@ucu.edu.ua>
2023-03-09 21:53:33 +0200

    Flask: now it's possible to open a lesson and complete some questions (currently questions are infinite, but you can quit by pressing "Return to main" button).

--- main.py
+++ main.py
@@ -2,26 +2,58 @@
 Backend of "Mnogomov" project
 """
 
+
 from flask import Flask, render_template
+import mysql.connector
+
 
 app = Flask(__name__, template_folder="../Mnogomov/templates")
 
 
+db = mysql.connector.connect(
+    host="localhost",
+    user="oigenoum",
+    password="BE_Cheporte_157E3_Legion",
+    database="lesson_data"
+)
+
+cursor = db.cursor()
+# Retrieve questions from db
+query = "SELECT * FROM question LIMIT 10"
+cursor.execute(query)
+questions = cursor.fetchall()
+# Shut down connection
+cursor.close()
+db.close()
+
+current_question = 0
+
+
 @app.route("/")
-@app.route("/home")
-def home_page():
+def home():
     """
     Show user the homepage
     """
     return render_template("home.html")
 
 
-@app.route("/lesson/")
-def lesson():
-    """
-    Show user the about page
-    """
-    return render_template('lesson.html')
+@app.route('/lesson')
+def display_question():
+    global current_question
+
+    # Check if we've reached the end of the questions list
+    if current_question >= len(questions):
+        # Reset the current_question index to 0
+        current_question = 0
+
+    # Get the current question from the list of questions
+    question = questions[current_question]
+
+    # Increment the current question index
+    current_question += 1
+
+    # Render the template with the question data
+    return render_template('lesson.html', question=question, current_question=current_question)
 
 
 if __name__ == '__main__':
